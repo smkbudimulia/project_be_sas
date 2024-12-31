c726404589fd0277d5c40e595f74b1bfb968bd76
yuktiid <go.yukti.id@gmail.com>
2024-12-31 10:07:46 +0700

    db

--- Database/ConfigDB.js
+++ Database/ConfigDB.js
@@ -2,10 +2,17 @@ const knex = require('knex')
 
 
 const connection = {
+    //local
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'absensisiswa'
+
+    //server online
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'absensisiswa'
+    user: 'u1583982_sas',
+    password: 'v56tuHC4etcvhb66&',
+    database: 'u1583982_sas'
 }
 
 const database = knex({
