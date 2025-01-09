de54b83abde8d3d89bdf689acbbe945122d6ac51
dimazzfathi <dimazzfathi@gmail.com>
2025-01-09 16:54:53 +0700

    db hahahaha

--- Database/ConfigDB.js
+++ Database/ConfigDB.js
@@ -3,9 +3,14 @@ const knex = require('knex')
 
 const connection = {
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'absensisiswa'
+    user: 'u1583982_sas',
+    password: 'O))_F^ML]2D%',
+    database: 'u1583982_sas'
+
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'absensisiswa'
 }
 
 const database = knex({
