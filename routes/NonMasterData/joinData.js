9be9864644c77eb541db6e33239c1a6fd77fcc27
dimazzfathi <dimazzfathi@gmail.com>
2025-02-21 14:20:02 +0700

    pulang

--- routes/NonMasterData/joinData.js
+++ routes/NonMasterData/joinData.js
@@ -33,6 +33,7 @@ router.get('/total-kelas-siswa', async (req, res) => {
                 conn.raw('COUNT(CASE WHEN absensi.keterangan = "Alpa" THEN 1 END) AS total_alpa'),
                 conn.raw('COUNT(CASE WHEN absensi.keterangan = "Sakit" THEN 1 END) AS total_sakit'),
                 conn.raw('COUNT(CASE WHEN absensi.keterangan = "Izin" THEN 1 END) AS total_izin'),
+                conn.raw('COUNT(CASE WHEN absensi.keterangan = "Pulang" THEN 1 END) AS total_pulang'),
             )
             .leftJoin('siswa', 'absensi.id_siswa', 'siswa.id_siswa')
             .leftJoin('kelas', 'siswa.id_kelas', 'kelas.id_kelas')
